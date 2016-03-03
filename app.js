5b15e22c332f62549a5f04373b93e839702c4028
Summer McDonald <summermichiko@gmail.com>
2016-03-02 22:05:13 -0800

    refactored code

--- app.js
+++ app.js
@@ -10,15 +10,12 @@ $(document).ready(function() {
 		score,
 
 		// wufooSaurus variables
-		wufooSaurusWidth = 145,
-		wufooSaurusHeight = 158,
-		wufooSaurusSpeed = 10,
 		wufooSaurus = {
+			width: 145,
+			height: 158,
 			x: 30,
-			y: canvas.height - wufooSaurusHeight,
-			width: wufooSaurusWidth,
-			height: wufooSaurusHeight,
-			wufooSaurusSpeed: wufooSaurusSpeed
+			y: canvas.height - 158,
+			wufooSaurusSpeed: 13
 		},
 
 		// survey variables
@@ -27,19 +24,6 @@ $(document).ready(function() {
 		totalSurveys = 7,
 		surveys = [],
 
-		// x variables
-		counter = 0;
-		xArr = [],
-		xWidth = 20,
-		xHeight = 20,
-		xIcon = {
-			x: 630,
-			y: 15,
-			width: xWidth,
-			height: xHeight
-		},
-
-		// helper object
 		helper = {
 			addSurvey: function() {
 				var survey = {
@@ -49,63 +33,29 @@ $(document).ready(function() {
 				helper.resetSurvey(survey);
 				surveys.push(survey);
 			},
-			resetSurvey: function(survey) {
-				// randomly position survey near the top of canvas
-				survey.x = Math.random() * (canvas.width - surveyWidth);
-				survey.y = 40 + Math.random() * 30;
-				survey.speed = (0.55 + Math.random()) * 0.9;
-			},
-			checkMisses: function() {
-				if (xArr.length == 3) {
-					continueAnimating = false;
-					$('#surveyModal').modal({
-						// backdrop: 'static',
-						// keyboard: false
-					});
-				}
-			},
 			animate: function() {
 				var sound;
-				// request another animation frame
 				if (continueAnimating) {
 					requestAnimationFrame(helper.animate);
 				}
-				// for each survey
-				// (1) check for collisions
-				// (2) advance the survey
-				// (3) if the survey falls below the canvas, reset that survey
-				for (var i = 0; i < surveys.length; i++) {
-					var survey = surveys[i];
-					if (helper.isColliding(survey, wufooSaurus)) {
-						sound = new Audio("audio/coinSound.mp3");
-						sound.play();
-						score += 5;
-						helper.resetSurvey(survey);
-					}
-					survey.y += survey.speed;
-					// if the survey is below the canvas,
-					if (survey.y > canvas.height) {
-						sound = new Audio("audio/buzzerSound.mp3");
-						sound.play();
-						helper.generateX();
-						helper.resetSurvey(survey);
-					}
-				}
-				// redraw everything
+				helper.trackFallingSurveys();
 				helper.drawAll();
 			},
-			isColliding: function(a, b) {
-				return !(b.x > a.x + a.width || b.x + b.width < a.x || b.y > a.y + a.height || b.y + b.height < a.y);
-			},
 			drawAll: function() {
-				// clear the canvas
-				context.clearRect(0, 0, canvas.width, canvas.height);
-
-				// draw the background
+				helper.drawCanvas();
+				helper.drawBackground();
+				helper.drawWufooSaurus();
+				helper.drawScore();
+				helper.drawSurveys();
+			},
+			drawBackground: function() {
 				context.fillStyle = '#fff';
 				context.fillRect(0, 0, canvas.width, canvas.height);
-
-				// draw wufooSaurus
+			},
+			drawCanvas: function() {
+				context.clearRect(0, 0, canvas.width, canvas.height);
+			},
+			drawWufooSaurus: function() {
 				var img = new Image();
 				img.src = 'assets/wufooSaurus.png';
 				function drawDino() {
@@ -115,48 +65,43 @@ $(document).ready(function() {
 					drawDino();
 				}
 				init();
-
-				// draw all surveys
-				function generateSurvey() {
-					var icon = new Image();
-					icon.src = 'assets/surveyIcon.png';
-					function drawSurvey() {
-						context.drawImage(icon, survey.x, survey.y, survey.width, survey.height);
-					}
-					function init() {
-						drawSurvey();
-					}
-					init();
-				}
-				for (var i = 0; i < surveys.length; i++) {
-					var survey = surveys[i];
-					generateSurvey();
-				}
-
-				// draw the score
+			},
+			drawScore: function() {
 				context.font = "14px 'Press Start 2P'";
 				context.fillStyle = "#000";
 				context.fillText("Score:" + score, 15, 30);
 			},
-			generateX: function() {
-				console.log('inside generateX func');
-				var imgX = new Image();
-				imgX.src = 'assets/redX.png';
-				function drawX() {
-					console.log(3);
-					counter+=20;
-					context.drawImage(imgX, xIcon.x + counter, xIcon.y, xIcon.width, xIcon.height);
-					xArr.push(imgX);
-					console.log('xArr', xArr);
+			drawSurveys: function() {
+				for (var i = 0; i < surveys.length; i++) {
+					var survey = surveys[i];
+					helper.generateSurvey(survey);
 				}
-				if (imgX.complete) {
-					console.log(1);
-					drawX();
-				} else {
-					console.log(2);
-					imgX.onload = drawX;
+			},
+			generateSurvey: function(survey) {
+				var icon = new Image();
+				icon.src = 'assets/surveyIcon.png';
+				function drawOneSurvey() {
+					context.drawImage(icon, survey.x, survey.y, survey.width, survey.height);
 				}
-				helper.checkMisses();
+				function init() {
+					drawOneSurvey();
+				}
+				init();
+			},
+			isColliding: function(a, b) {
+				return !(b.x > a.x + a.width || b.x + b.width < a.x || b.y > a.y + a.height || b.y + b.height < a.y);
+			},
+			logMiss: function() {
+				continueAnimating = false;
+				$('#surveyModal').modal({
+					backdrop: 'static',
+					keyboard: false
+				});
+			},
+			resetSurvey: function(survey) {
+				survey.x = Math.random() * (canvas.width - surveyWidth);
+				survey.y = 40 + Math.random() * 30;
+				survey.speed = (0.55 + Math.random()) * 0.9;
 			},
 			setGame: function() {
 				score = startingScore;
@@ -170,37 +115,35 @@ $(document).ready(function() {
 					helper.animate();
 				}
 			},
-			// flip: function() {
-			// 	var flipH = document.getElementById('horizontalCheckbox').checked,
-			// 		flipV = document.getElementById('verticalCheckbox').checked;
-			// 	flipImage(img, ctx, flipH, flipV);
-			// 	return false;
-			// },
-			flipImage: function(image, ctx, flipH, flipV) {
-				var scaleH = -1, // Set horizontal scale to -1 if flip horizontal
-					scaleV = 1, // Set verical scale to -1 if flip vertical
-					posX = flipH ? width * -1 : 0, // Set x position to -100% if flip horizontal
-					posY = flipV ? height * -1 : 0; // Set y position to -100% if flip vertical
-
-				context.scale(scaleH, scaleV); // Set scale to flip the image
-				context.drawImage(img, posX, posY, width, height); // draw the image
-				context.restore(); // Restore the last saved state
+			trackFallingSurveys: function() {
+				for (var i = 0; i < surveys.length; i++) {
+					var survey = surveys[i];
+					if (helper.isColliding(survey, wufooSaurus)) {
+						sound = new Audio("audio/coinSound.mp3");
+						sound.play();
+						score += 5;
+						helper.resetSurvey(survey);
+					}
+					survey.y += survey.speed;
+					// if the survey is below the canvas,
+					if (survey.y > canvas.height) {
+						sound = new Audio("audio/buzzerSound.mp3");
+						sound.play();
+						helper.logMiss();
+					}
+				}
 			}
 		};
 
 	//left and right keypush event handlers
 	document.onkeydown = function (event) {
-		// right arrow
 		if (event.keyCode == 39) {
 			wufooSaurus.x += wufooSaurus.wufooSaurusSpeed;
-			// helper.flipImage(wufooSaurus, context, -1, 1);
-
 			if (wufooSaurus.x >= canvas.width - wufooSaurus.width) {
 				wufooSaurus.x = canvas.width - wufooSaurus.width;
 			}
 		} else if (event.keyCode == 37) {
 			wufooSaurus.x -= wufooSaurus.wufooSaurusSpeed;
-
 			if (wufooSaurus.x <= 0) {
 				wufooSaurus.x = 0;
 			}
@@ -211,6 +154,7 @@ $(document).ready(function() {
 	for (var i = 0; i < totalSurveys; i++) {
 		helper.addSurvey();
 	}
+
 	app.on('click', '.button1', function() {
 		var instructionView = $(this).closest('.surgeView');
 		var nextView = app.find('.instruction2');
@@ -224,7 +168,6 @@ $(document).ready(function() {
 		nextView.show();
 	});
 
-	// button to start the game
 	app.on('click', '#start', function() {
 		var instructionView = $(this).closest('.surgeView');
 		var nextView = app.find('.surveyGame');
